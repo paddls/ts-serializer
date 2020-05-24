a81d3c72d33ecd8ce9f238cba3112b00949f6c1a
Thomas Nisole <thomas.nisole@witty-services.fr>
2020-05-24 18:33:15 +0200

    feat: Add some type in public api

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 export {Converter} from './converter/converter';
 export {DateConverter} from './converter/date.converter';
 
-export {Column, ColumnContext} from './decorator/column.decorator';
+export {Column, ColumnContext, ColumnContextConfiguration, COLUMNS_METADATA_KEY} from './decorator/column.decorator';
 
 export {Denormalizer} from './normalizer/denormalizer';
 export {NormalizerConfiguration, DEFAULT_NORMALIZER_CONFIGURATION} from './normalizer/normalizer.configuration';
