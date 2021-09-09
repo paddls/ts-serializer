bea5cb956efdaa82f36b43134c2c49cac3dd589b
thomasnisole <thomasnisole@gmail.com>
2021-09-09 17:06:30 +0200

    release: v1.0.7-rc.5

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  preset: 'ts-jest',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec|spec-e2e))\\.ts?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
