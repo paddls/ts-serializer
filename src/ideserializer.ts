ad77219531c74f8f914bbb6d09c1245a852df911
Thomas Nisole <thomasnisole@gmail.com>
2022-01-14 15:36:59 +0100

    fix: Typing ideserializer interface

--- src/ideserializer.ts
+++ src/ideserializer.ts
@@ -3,7 +3,7 @@ import {SerializerOptions} from './serializer-options';
 
 export interface IDeserializer {
 
-  deserialize<T>(type: SerializeType<T>|SerializeType<any>[], data: any, options?: SerializerOptions): T;
+  deserialize<T>(type: SerializeType<T>|SerializeType<any>[], data: any, options?: SerializerOptions): T|null;
 
   deserializeAll<T>(type: SerializeType<T>|SerializeType<any>[], data: any[], options?: SerializerOptions): T[];
 }
